e5bfd0ca4a1089163183154805daefa279f2d9f9
Harry Larimer <harrylarimerjr@gmail.com>
2022-09-01 10:32:37 -0500

    Add JavaScript operation/clear functionality
    * Add event listeners for operators and clear button
    * Add operator functionality - calculator now works for basic operations
    * Add functionality for "=" button
    * Add clear button functionality - works for "C" and "AC"
    * Add changing text to clear button

--- script.js
+++ script.js
@@ -1,28 +1,95 @@
 const calcDisplay = document.querySelector('.calcDisplay');
-const displayDefault = 0;
-let displayNums = displayDefault;
+const calcDisplayDefault = 0;
+let displayValue = "";
+const clearBtn = document.getElementById('clearBtn');
 
-const clearBtn = document.querySelector('#clearBtn');
+let runningTotal = null;
+let secondNum = null;
+let currOperator = null;
+let allowDecimal = true;
 
-
-function clickFunction(btnText) {
-    if (calcDisplay.textContent === '0') {
-        calcDisplay.textContent = btnText;
-        return;
+function clearButton () {
+    if (runningTotal !== null) {
+        secondNum = null;
+        displayValue = "";
+        calcDisplay.textContent = calcDisplayDefault;
+        clearBtn.textContent = "AC";
     }
     else {
-        calcDisplay.textContent += btnText
-        return;
+        runningTotal = null;
+        currOperator = null;
+        allowDecimal = true;
+        displayValue = "";
+        calcDisplay.textContent = calcDisplayDefault;
+        clearBtn.textContent = "AC";
     }
 }
 
-const addNumEventListeners = function() {
-    calcDisplay.textContent = displayNums;
+function operatorClick(operatorText) {
+    if (runningTotal === null) {
+        runningTotal = calcDisplay.textContent;
+        displayValue = "";
+    }
+    else {
+        secondNum = calcDisplay.textContent;
+    }
+
+    if ((runningTotal !== null) && (secondNum !== null)) {
+        runningTotal = operate(currOperator, runningTotal, secondNum);
+        calcDisplay.textContent = runningTotal;
+        displayValue = "";
+        secondNum = null;
+    }
+    currOperator = operatorText;
+    if (currOperator === "=") {
+        runningTotal = null;
+    }
+}
+
+function numClick(btnText) {
+        if (btnText === ".") {
+            if (allowDecimal === true) {
+                displayValue = "0" + btnText;
+                allowDecimal = false;
+            }
+        }
+        else {
+            displayValue += btnText;
+        }
+        calcDisplay.textContent = displayValue;
+        clearBtn.textContent = "C";
+        return;
+}
+
+function addAllEventListeners() {
+    addClearEventListener();
+    addOperatorEventListeners();
+    addNumEventListeners();
+}
+
+function addClearEventListener() {
+    clearBtn.addEventListener('click', () => {
+        clearButton();
+    })
+}
+
+function addOperatorEventListeners() {
+    const operatorBtns = document.querySelectorAll('.functionBtns');
+    const operatorBtnArray = [...operatorBtns];
+    operatorBtnArray.forEach(element => {
+        element.addEventListener('click', () => {
+            operatorClick(element.textContent);
+        });
+    });
+}
+
+function addNumEventListeners() {
+    calcDisplay.textContent = calcDisplayDefault;
     const numBtns = document.querySelectorAll('.numBtns');
     const numBtnArray = [...numBtns];
     numBtnArray.forEach(element => {
         element.addEventListener('click', () => {
-            clickFunction(element.textContent);
+            numClick(element.textContent);
         });
     });
 }
@@ -45,17 +112,19 @@ function divideNums(num1, num2) {
 
 function operate(operatorStr, num1, num2) {
     let answer;
+    const a = parseFloat(num1);
+    const b = parseFloat(num2);
     if (operatorStr === "+") {
-        answer = addNums(num1, num2);
+        answer = addNums(a, b);
     }
-    else if (operatorStr === "-") {
-        answer = subtractNums(num1, num2);
+    else if (operatorStr === "−") {
+        answer = subtractNums(a, b);
     }
-    else if (operatorStr === "*") {
-        answer = multiplyNums(num1, num2);
+    else if (operatorStr === "×") {
+        answer = multiplyNums(a, b);
     }
-    else if (operatorStr === "/") {
-        answer = divideNums(num1, num2);
+    else if (operatorStr === "÷") {
+        answer = divideNums(a, b);
     }
     else {
         return "ERROR";
@@ -63,4 +132,40 @@ function operate(operatorStr, num1, num2) {
     return answer;
 }
 
-addNumEventListeners();
+addAllEventListeners();
+
+// OLD CODE - DOES NOT WORK PROPERLY
+
+// let operatorActive = false;
+
+
+// function operatorClick(operatorText) {
+//     if (operatorActive === false) {
+//         storedDisplay = displayValue;
+//         displayValue = "";
+//         operatorActive = true;
+//         currOperator = operatorText;
+//     }
+//     else {
+//         displayValue = operate(currOperator, storedDisplay, displayValue);
+//         calcDisplay.textContent = displayValue;
+//     }
+// }
+
+
+// function numClick(btnText) {
+//     if (calcDisplay.textContent === '0') {
+//         displayValue += btnText;
+//         calcDisplay.textContent = displayValue;
+//         return;
+//     }
+//     else {
+//         if (operatorActive) {
+//             displayValue = "";
+//             calcDisplay.textContent = displayValue;
+//         }
+//         displayValue += btnText;
+//         calcDisplay.textContent = displayValue;
+//         return;
+//     }
+// }
